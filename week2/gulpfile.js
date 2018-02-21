bc62061a43ec4176f93880bf7d2206be715dd45b
Kangyun Wang <wky@029.157.028.145.hva.nl>
2018-02-21 13:23:01 +0100

    Added a week2 map with what has been done there

---
+++ week2/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var beautify = require('gulp-cssbeautify');
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function () {
+	gulp
+		.src('./public/styles/*.scss')
+		.pipe(sass({ includePaths: ['scss'] }))
+		.pipe(beautify())
+		.pipe(gulp.dest('./public/styles'));
+});
+
+gulp.task('browser-sync', function () {
+	browserSync.init(['*.html', './public/styles/styles.css'], {
+		server: {
+			baseDir: './'
+		},
+		port: 4000,
+		ui: {
+			port: 4001
+		}
+	});
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function () {
+	gulp.watch('./public/styles/*.scss', ['sass']); // Watching all scss changes on changes in the background
+});
